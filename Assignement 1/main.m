74ffe7dbe8b7eeddc8e122eb04989c1d087a200c
Mario <mrm994@gmail.com>
2017-11-27 18:57:55 +0100

    Plot hyperplane

--- Assignement 1/main.m
+++ Assignement 1/main.m
@@ -1,11 +1,16 @@
 cla;clc;clear;
-P = 3;
+P = 15;
 N = 2;
 [xi, S] = generate_data(P, N);
 w = train(xi, S, 1000);
 
+%% plotting
+
 figure;
 hold on;
+
+axis([min(xi(:))-1, max(xi(:))+1, min(xi(:))-1, max(xi(:))+1]);
+pbaspect([1, 1, 1]);
 
 for i = 1:P
     if S(i) > 0
@@ -17,4 +22,8 @@ end
 
 h = null(w(:).')
 
-axis([min(xi(:))-1, max(xi(:))+1, min(xi(:))-1, max(xi(:))+1])
+% plot h
+xlim = get(gca,'XLim');
+% m = h(2)/h(1);
+m = h(1)/h(2);
+line([xlim(1), xlim(2)], [m*xlim(1), m*xlim(2)]);

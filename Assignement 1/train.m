d324b6c47f7493baec98e8825b452bf57e68652d
Mario <mrm994@gmail.com>
2017-11-29 11:59:11 +0100

    Fixed bug that prevented the perceptron to converge

--- Assignement 1/train.m
+++ Assignement 1/train.m
@@ -1,8 +1,8 @@
 function [ w, stop ] = train( xi, S, limit)
     P = size(xi, 2);
     w = zeros(size(xi,1),1);
-    stop = true;
     for t=1:limit;
+        stop = true;
         for j=1:P
            [w, E] = rosenblatt(w, xi(:,j), S(j));
            stop = stop & (E > 0);

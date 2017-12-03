c0aed6f2201f5b59334a8c04bbff538d075ff6e5
m-rios <mrm994@gmail.com>
2017-12-03 21:33:59 +0100

    First results

--- Assignement 1/main.m
+++ Assignement 1/main.m
@@ -8,6 +8,7 @@ aQs = zeros(1,size(alpha,2));
 bQs = zeros(1,size(alpha,2));
 cQs = zeros(1,size(alpha,2));
 dQs = zeros(1,size(alpha,2));
+load('data');
 %% e) + a)
 thQs = theoreticQs(alpha, N);
 aQs = getQs(alpha, N, nD, nMax);
@@ -24,11 +25,14 @@ dQs = getQs(alpha, N, nD, nMax);
 %% Plot
 cla;
 hold on;
+stairs(alpha, aQs, 'Color', 'red', 'DisplayName', 'N=20 nMax=100', 'LineStyle','--', 'MarkerFaceColor', 'red', 'Marker', 'o');
+stairs(alpha, bQs, 'Color', 'green', 'DisplayName', 'N = 50 nMax=150;', 'LineStyle','--', 'MarkerFaceColor', 'green', 'Marker', 'x');
+stairs(alpha, cQs, 'Color', 'blue', 'DisplayName', 'N = 100 nMax=300', 'LineStyle','--', 'MarkerFaceColor', 'blue', 'Marker', 'v');
+stairs(alpha, dQs, 'Color', 'magent', 'DisplayName', 'N = 200 nMax=600', 'LineStyle','--', 'MarkerFaceColor', 'magent', 'Marker', 'd');
 stairs(alpha, thQs, 'Color', 'black', 'DisplayName', 'theoretical');
-stairs(alpha, aQs, 'Color', 'red', 'DisplayName', 'N=20 nMax=100', 'LineStyle','--', 'MarkerFaceColor', 'red', 'Marker', 'd');
-stairs(alpha, bQs, 'Color', 'green', 'DisplayName', 'N = 50 nMax=150;');
-stairs(alpha, cQs, 'Color', 'blue', 'DisplayName', 'N = 100 nMax=300');
-stairs(alpha, dQs, 'Color', 'yellow', 'DisplayName', 'N = 200 nMax=600');
 grid on;
-legend;
+legend('show');
+title('Probability of linear separability as a function of P/N')
+ylabel('Probability')
+xlabel('P/N');
 axis([0.5 3 -0.01 1.01])

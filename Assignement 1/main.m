38023faf2e4a064f096cd6f891c71c90827ccb86
m-rios <mrm994@gmail.com>
2017-12-06 20:12:08 +0100

    Plot for N=20 with incremental nmax

--- Assignement 1/main.m
+++ Assignement 1/main.m
@@ -3,44 +3,36 @@ clear; clc; cla;
 alpha = [.75:.25:3];
 N = 20;
 nD = 400;
-nMax = 100;
+nMax = 1500;
 aQs = zeros(1,size(alpha,2));
 bQs = zeros(1,size(alpha,2));
 cQs = zeros(1,size(alpha,2));
 dQs = zeros(1,size(alpha,2));
-load('data');
+load('nmax-1500');
 %% e) + a)
 thQs = theoreticQs(alpha, N);
-aQs = getQs(alpha, N, nD, nMax);
-
+% aQs = getQs(alpha, N, nD, nMax);
 %% b)
-N = 50; nMax=150;
+nMax=100;
 bQs = getQs(alpha, N, nD, nMax);
 %% c)
-N = 100; nMax=300;
+nMax=500;
 cQs = getQs(alpha, N, nD, nMax);
 %% d)
-N = 200; nMax=600;
-dQs = getQs(alpha, N, nD, nMax);
+% nMax=600;
+% dQs = getQs(alpha, N, nD, nMax);
 %% Plot
 cla;
 hold on;
-line(alpha, aQs, 'Color', 'red', 'DisplayName', 'N=20 nMax=100', 'LineStyle','--', 'MarkerFaceColor', 'red', 'Marker', 'o');
+line(alpha, bQs, 'Color', 'green', 'DisplayName', 'nMax=100', 'MarkerFaceColor', 'green', 'Marker', 'd');
+line(alpha, cQs, 'Color', 'blue', 'DisplayName', 'nMax=500', 'MarkerFaceColor', 'blue', 'Marker', 'V');
+line(alpha, aQs, 'Color', 'red', 'DisplayName', 'nMax=1500', 'MarkerFaceColor', 'red', 'Marker', 'o');
+% line(alpha, dQs, 'Color', 'magent', 'DisplayName', 'N = 200 nMax=600', 'LineStyle','--', 'MarkerFaceColor', 'magent', 'Marker', 'd');
 line(alpha, thQs, 'Color', 'black', 'DisplayName', 'theoretical');
 grid on;
 legend('show');
-title('Probability of linear separability as a function of P/N')
+title('Probability of linear separability as a function of P/N, N = 20')
 ylabel('Probability')
 xlabel('P/N');
-figure
-hold on
-line(alpha, bQs, 'Color', 'green', 'DisplayName', 'N = 50 nMax=150;', 'LineStyle','--', 'MarkerFaceColor', 'green', 'Marker', 'x');
-line(alpha, cQs, 'Color', 'blue', 'DisplayName', 'N = 100 nMax=300', 'LineStyle','--', 'MarkerFaceColor', 'blue', 'Marker', 'v');
-line(alpha, dQs, 'Color', 'magent', 'DisplayName', 'N = 200 nMax=600', 'LineStyle','--', 'MarkerFaceColor', 'magent', 'Marker', 'd');
-line(alpha, thQs, 'Color', 'black', 'DisplayName', 'theoretical');
-grid on;
-legend('show');
-title('Probability of linear separability as a function of P/N')
-ylabel('Probability')
-xlabel('P/N');
-axis([1.25 2.5 -0.01 1.01])
+axis([1 3 -0.01 1.01])
+print('incremental-nmax','-dpng','-r500')

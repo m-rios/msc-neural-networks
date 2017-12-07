9a90923c6606bfada4cbb8ba7f0a572fa48db9ee
m-rios <mrm994@gmail.com>
2017-12-07 22:59:02 +0100

    Changed font size in the plots

--- Assignement 1/main.m
+++ Assignement 1/main.m
@@ -21,15 +21,16 @@ bQs = getQs(alpha, 60, nD, 1500, 0);
 %% Plot
 cla;
 hold on;
-line(alpha, bQs, 'Color', 'green', 'DisplayName', 'N=60 nMax=1500', 'MarkerFaceColor', 'green', 'Marker', 'd');
-line(alpha, cQs, 'Color', 'blue', 'DisplayName', 'N = 20 nMax=500', 'MarkerFaceColor', 'blue', 'Marker', 'V');
+line(alpha, cQs, 'Color', 'blue', 'DisplayName', 'N = 20 nMax=500', 'MarkerFaceColor', 'blue', 'Marker', 'V', 'LineWidth', 2);
 line(alpha, aQs, 'Color', 'red', 'DisplayName', 'N= 40 nMax=1000', 'MarkerFaceColor', 'red', 'Marker', 'o');
+line(alpha, bQs, 'Color', 'green', 'DisplayName', 'N=60 nMax=1500', 'MarkerFaceColor', 'green', 'Marker', 'd');
 % line(alpha, dQs, 'Color', 'magent', 'DisplayName', 'N = 200 nMax=600', 'MarkerFaceColor', 'magent', 'Marker', 's');
-line(alpha, thQs, 'Color', 'black', 'DisplayName', 'theoretical');
+% line(alpha, thQs, 'Color', 'black', 'DisplayName', 'theoretical');
 grid on;
-legend('show');
-title('Probability of linear separability as a function of P/N, N = 20, nmax = 5001')
-ylabel('Probability')
-xlabel('P/N');
+lgd = legend('show', 'FontSize', 10);
+lgd.FontSize = 15;
+title('Probability of linear separability as a function of P/N, N = 20, nmax = 500', 'FontSize', 16)
+ylabel('Probability', 'FontSize', 20)
+xlabel('P/N', 'FontSize', 20);
 axis([1 3 -0.01 1.01])
-% print('incremental-nmax','-dpng','-r500')
+print('bonus-1','-dpng','-r500')

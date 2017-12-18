bb2dfbffbf7ee923149adb8a4bbe3439b637ce35
m-rios <mrm994@gmail.com>
2017-12-18 12:46:47 +0100

    Added irish dataset classification and 2 runs for minover

---
+++ Assignment 2/iris.m
@@ -0,0 +1,36 @@
+clear; cla; clc;
+load('fisheriris');
+
+or_xi = meas(:, 3:4)';
+or_xi(3, :) = -1;
+or_S = grp2idx(species);
+or_S(or_S==2) = -1;
+or_S(or_S==3) = -1;
+
+r = randperm(length(or_S));
+
+xi = or_xi(:,r);
+S = or_S(r);
+
+
+w = minover(xi(:,1:floor(length(xi)*0.8)), S(1:floor(length(S)*0.8)), 1000);
+
+hit = 0;
+
+parfor i = floor(length(xi)*0.8)+1:length(xi)
+    hit = hit + (sign(dot(w,xi(:,i))) == S(i));
+end
+
+hit_rate = hit/length(floor(length(xi)*0.8)+1:length(xi));
+
+
+[row, col] = find(or_S ==-1);
+figure, hold on;
+scatter(or_xi(1,1:min(row)-1), or_xi(2,1:min(row)-1), 'Marker', '+', 'DisplayName', 'Setosa');
+scatter(or_xi(1,min(row):length(or_xi)), or_xi(2,min(row):length(or_xi)), 'Marker', 'o', 'DisplayName', 'Versicolor & Virginica');
+y = @(x) (w(3) - w(1)*x)/w(2);
+x = linspace(0,8);
+line(x, y(x), 'DisplayName', 'Classifier');
+title('Minover solution for the fisher iris dataset');
+legend('Location','northwest');
+axis([0 8 -1 3]);
